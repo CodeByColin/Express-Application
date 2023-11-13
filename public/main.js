59ccefd8abdeb3e2b371c338a1e7a38ad22bfde0
Colin Tolliver <colintolliver@gmail.com>
2023-11-13 15:18:54 -0800

    CRUD server functionality

---
+++ public/main.js
@@ -0,0 +1,5 @@
+
+const div = document.createElement("div");
+
+div.textContent = "lets gooooo"
+document.body.appendChild(div)
--- server.js
+++ server.js
@@ -16,6 +16,8 @@ const pool = new Pool({
     port: 5432,
 });
 
+app.use(express.static('public'))
+
 app.use(express.json());
 
 app.get('/api/:resource/:id?', async (req, res) => {
@@ -42,8 +44,92 @@ app.get('/api/:resource/:id?', async (req, res) => {
       res.status(500).send('Server Error');
     }
   });
-  
 
+  app.post('/api/:resource', async (req, res) => {
+    const resource = req.params.resource;
+    const data = req.body;
+  
+    try {
+      let query;
+      let result;
+  
+      if (resource === 'members') {
+        query = 'INSERT INTO members (firstname, lastname, membership) VALUES ($1, $2, $3) RETURNING *';
+        result = await pool.query(query, [data.firstname, data.lastname, data.membership]);
+      } else if (resource === 'books') {
+        query = 'INSERT INTO books (title, author, genre, available) VALUES ($1, $2, $3, $4) RETURNING *';
+        result = await pool.query(query, [data.title, data.author, data.genre, data.available]);
+      } else {
+        res.status(404).send('Invalid Entry');
+        return;
+      }
+  
+      res.status(201).json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Server Error');
+    }
+  });
+
+  app.put('/api/:resource/:id', async (req, res) => {
+    const resource = req.params.resource;
+    const data = req.body;
+    const id = req.params.id;
+
+    try {
+        let query;
+        let result;
+
+        if (resource === 'members') {
+            query = 'UPDATE members SET firstname = $1, lastname = $2, membership = $3 WHERE id = $4 RETURNING *'
+            result = await pool.query(query, [data.firstname, data.lastname, data.membership, id])
+        } else if (resource ==='books') {
+            query = 'UPDATE books SET title = $1, author = $2, genre = $3, available = $4 WHERE id = $5 RETURNING *'
+            result = await pool.query(query, [data.title, data.author, data.genre, data.available, id]);
+        }else{
+            res.status(404).send('Not Found');
+            return;
+        }
+
+        if (result.rows.length === 0) {
+            res.status(404).send('Not Found');
+            return;
+        }
+
+        res.status(200).json(result.rows[0]);
+    } catch (err) {
+        res.status(500).send('Server Error')
+    }
+  })
+
+  app.delete('/api/:resource/:id', async (req, res) => {
+    const resource = req.params.resource;
+    const id = req.params.id;
+
+    try {
+        let query;
+        let result;
+
+        if(resource === 'members') {
+            query = 'DELETE FROM members WHERE id = $1 RETURNING *'
+            result = await pool.query(query, [id]);
+        }else if (resource === 'books') {
+            query = 'DELETE FROM books WHERE id = $1 RETURNING *'
+            result = await pool.query(query, [id]);
+        } else {
+            res.status(404).send('Not Found')
+            return;
+        }
+
+        if (result.rows.length === 0) {
+            res.status(404).send('Not Found')
+            return;
+        }
+        res.status(204).send();
+    } catch (err) {
+        res.status(500).send('Server Error')
+    }
+  })
 
 
 app.listen(PORT, () => {
